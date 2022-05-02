c0a893b1d2ac530be94d566bbce7ec4bb195d246
Kevin Nielsen <knielsen24@gmail.com>
2022-05-01 20:57:28 -0700

    commit tree 3c57aefccd1f0cab43b5827287100fe3b79e1c94

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+const removeMain = () => {
+    const main = document.querySelector('#main')
+    main.remove()
+}
+
+removeMain()
+
+// const newHeader = () => {
+//     const h1Victory = document.createElement('h1')
+//     return h1Victory.setAttribute('id', 'victory')
+// }
+
+// newHeader()
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = "<h1> is the champion</h1>"
